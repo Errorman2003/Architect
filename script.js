4ed97b3b741dd892f684030681afa2c531b6b648
Amobi Jonathan Chukwudi <82114500+Jonathanthedeveloper@users.noreply.github.com>
2022-12-21 20:57:24 +0100

    added a few animations

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 
+
+// ELEMENTS
 const button = document.querySelector('.navigation__toggle');
 const buttonSpan = document.querySelector('.navigation__toggle').children[0]
-console.log(buttonSpan);
+const sidebar = document.querySelector('.navigation__sidebar');
 
-const sidebar = document.querySelector('.navigation__sidebar')
+const navbar = document.querySelector('.navigation');
+const main = document.querySelector('.main');
+const allSections = document.querySelectorAll('.scroll-animate')
+
+
 
 function toggleSidebar(){
     sidebar.classList.toggle('navigation__sidebar--open');
@@ -16,5 +22,41 @@ function toggleSidebar(){
 }
 
 
+// EVENT LISTENER
+button.addEventListener('click', toggleSidebar);
 
-button.addEventListener('click', toggleSidebar)
+// STICKY NAVIGATION BAR
+const fixNavbar = function(entries,observe){
+const [entry] = entries;
+if(entry.isIntersecting){
+    navbar.classList.add('navigation__fixed')
+} else {
+    navbar.classList.remove('navigation__fixed')
+}
+
+}
+
+const observeMain = new IntersectionObserver(fixNavbar, {
+    root: null,
+    threshold: 0.1
+})
+observeMain.observe(main);
+
+
+// Come into view
+const slideUp = function(entries, observer){
+    const [entry] = entries;
+if(!entry.isIntersecting) return
+
+    entry.target.classList.add('scroll-animate--visible')
+    
+    observer.unobserve(entry.target)
+
+}
+
+const observeSection = new IntersectionObserver(slideUp, {
+    root: null,
+    threshold: 0
+})
+
+allSections.forEach(section => observeSection.observe(section))
